1e8a8f6455d02b177d111f265990647ff9d8ba67
eunhyePark <eunhye0406@gmail.com>
2019-09-24 00:18:23 +0900

    home 초기설정 및 메인 마크업

---
+++ docs/js/index.js
@@ -0,0 +1,13 @@
+//main slider
+
+var swiper = new Swiper('.swiper-container', {
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
